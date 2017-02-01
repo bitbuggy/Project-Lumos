5dd26d03f67f23f48c1cc81f46b3a1b12f0f75f9
Brenno Tondato de Faria <brenno.tondato@gmail.com>
2017-02-01 18:48:17 -0200

    Bluetooth Control

--- src/LumosScript/LumosScript.ino
+++ src/LumosScript/LumosScript.ino
@@ -1,6 +1,8 @@
-//BRAIN LABS 9/01/2017
-//Projeto LUMOS
+//Bit Buggy 01/09/2017
+//Project LUMOS
 
+
+#include <SoftwareSerial.h>
 #include<string.h>
 
 const int TouchPin = 6;
@@ -8,17 +10,30 @@ const int ledPin = 3;
 const int relayPin = 8;
 bool ledstate = false;
 bool flag = false;
-int serialData; 
+int serialData;
+int serialDatab; 
+
+void controlRele(int control){
+  if(control == 101){
+     ledstate = !ledstate;
+     digitalWrite(relayPin, ledstate);
+     digitalWrite(ledPin, ledstate);
+     Serial.println(serialData);
+   }
+}
+
 
 void setup(){ 
   pinMode(TouchPin, INPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(relayPin, OUTPUT);
   Serial.begin(9600);
+  Serial1.begin(38400);
 }
 
 void loop() {
   int sensorValue = digitalRead(TouchPin);
+  
   if(sensorValue == 1){
     if (flag == false) {
       ledstate = !ledstate;
@@ -31,15 +46,15 @@ void loop() {
   else {
     flag = false;
   }
+  
   if(Serial.available() > 0){
     serialData = Serial.read();
-    
-    if(serialData == 101){
-      ledstate = !ledstate;
-      digitalWrite(relayPin, ledstate);
-      digitalWrite(ledPin, ledstate);
-      Serial.println(serialData);
-    }
-   }
+    controlRele(serialData);
+  }
+
+  if(Serial1.available() > 0){
+    serialDatab = Serial1.read();
+    controlRele(serialDatab);
+  }
     delay(100);
  }
